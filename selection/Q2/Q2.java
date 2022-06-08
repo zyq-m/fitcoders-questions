13fe0874daca0d68d3500a295fe9cbc2e3cc02a8
Haziq Musa <last.game01987@gmail.com>
2022-06-09 03:08:26 +0800

    commit

--- selection/Q2/Q2.java
+++ selection/Q2/Q2.java
@@ -1,15 +1,16 @@
 package selection.Q2;
+
 import java.util.Scanner;
 
 public class Q2 {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System. in);
-        int num1 = input.nextInt():
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
         int num2 = input.nextInt();
         int num3 = input.nextInt();
         int num4 = input.nextInt();
         int num5 = input.nextInt();
-        
+
         if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
             System.out.println(num1 + " is the highest.");
         }
