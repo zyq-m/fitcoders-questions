82a38770dc3c0fc0e118fb4c5ff7a9b9d4e9e74b
Haziq Musa <last.game01987@gmail.com>
2022-06-09 03:50:53 +0800

    Update array and string Q2

--- arrayAndString/Q3/Q3.java
+++ arrayAndString/Q3/Q3.java
@@ -6,6 +6,11 @@ public class Q3 {
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         int arrSize = k.nextInt();
+
+        if (arrSize > 10) {
+            arrSize = 10;
+        }
+
         int[] num = new int[arrSize];
 
         for (int i = 0; i < num.length; i++) {
